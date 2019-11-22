56d18095c55d972bdb46ac57358da67a1a680091
Jonathan Norman <jnorman2123@gmail.com>
2019-11-22 18:50:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     user = User.new(:username => params[:username], :password => params[:password])
 
-    if user.password != nil && user.username != nil
+    if user.save
       redirect "/login"
     else
       redirect "/password"
