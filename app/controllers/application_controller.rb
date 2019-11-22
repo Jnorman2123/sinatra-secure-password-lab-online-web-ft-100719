932e67f1e5ef8dfc0eef56abc22cb831377a5ef1
Jonathan Norman <jnorman2123@gmail.com>
2019-11-22 18:57:03 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
 
     if user.save
       redirect "/login"
-    elsif user.username == nil || user.password == nil
+    else
       redirect "/failure"
     end
   end
